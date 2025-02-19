dcd72f3868e846dccf803ebc2f2193dfce299cd7
VKo65 <volker@kolauch.de>
2025-02-19 17:26:08 +0100

    New functions, for add, delete and update

---
+++ backend/backend_app.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
+
+POSTS = [
+    {"id": 1, "title": "First post", "content": "This is the first post."},
+    {"id": 2, "title": "Second post", "content": "This is the second post."},
+]
+
+
+@app.route('/api/posts', methods=['GET'])
+def get_posts():
+    return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    data = request.get_json()
+    # Error handling
+    if not data or 'title' not in data or 'content' not in data:
+        return jsonify({"error": "Title and content are required"})
+    new_id = max(post['id'] for post in POSTS) + 1 if POSTS else 1
+    new_post = {
+        "id": new_id,
+        "title": data['title'],
+        "content": data['content']
+    }
+
+    POSTS.append(new_post), 201
+    return jsonify(new_post), 201
+
+
+@app.route('/api/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+    global POSTS
+    post = next((post for post in POSTS if post["id"] == post_id), None)
+
+    if post is None:
+        return jsonify({"message": f"Post with id{post_id} not found!"}), 404
+
+    POSTS = [post for post in POSTS if post["id"] != post_id]
+
+    return jsonify({"message": f"Post with id {post_id} has been deleted successfully."}), 200
+
+
+@app.route('/api/posts/<int:post_id>', methods=['PUT'])
+def update_post(post_id):
+    post = next((post for post in POSTS if post["id"] == post_id), None)
+
+    if post is None:
+        return jsonify({"message": f"Post with id{post_id} not found!"}), 404
+
+    data = request.get_json()
+    post["title"] = data.get("title", post["title"])
+    post["content"] = data.get("content", post["content"])
+
+    return jsonify(post), 200
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5002, debug=True)
